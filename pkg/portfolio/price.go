027a53a21095d3f44d41384a55b2e925fbf16201
Anton Nefedov <a.nefedov@yahoo.com>
2020-07-30 10:18:50 +0300

    price: add annual percentage calculation

--- pkg/portfolio/price.go
+++ pkg/portfolio/price.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"../aux"
 	"../candles"
 	"../client"
 	"../schema"
@@ -17,14 +18,17 @@ func GetPrices(c *client.MyClient, tickers []string, start, end time.Time) {
 		p1 := cc.GetOnDay(ins.Figi, start)
 		p2 := cc.GetOnDay(ins.Figi, end)
 
-		s := fmt.Sprintf("%s: %.2f -> %.2f (%.1f%% %s)",
-			ticker, p1, p2, (p2/p1-1)*100, ins.Currency)
+		s := fmt.Sprintf("%s: %.2f -> %.2f (%.1f%% %s; %.1f%% annual)",
+			ticker, p1, p2, aux.Ratio2Perc(p2/p1), ins.Currency,
+			aux.Ratio2Perc(aux.RatioAnnual(p2/p1, end.Sub(start))))
 
 		if ins.Currency != "RUB" {
 			p1 *= cc.GetOnDay(schema.FigiUSD, start)
 			p2 *= cc.GetOnDay(schema.FigiUSD, end)
 
-			s += fmt.Sprintf(" (%.1f%% RUB)", (p2/p1-1)*100)
+			s += fmt.Sprintf(" (%.1f%% RUB; %.1f%% annual)",
+				aux.Ratio2Perc(p2/p1),
+				aux.Ratio2Perc(aux.RatioAnnual(p2/p1, end.Sub(start))))
 		}
 
 		fmt.Println(s)

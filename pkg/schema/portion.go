0cd1dce55f9ced88dfb5da741231b29f7333039c
Anton Nefedov <a.nefedov@yahoo.com>
2020-11-11 17:50:03 +0300

    schema/portion: print alpha balance

--- pkg/schema/portion.go
+++ pkg/schema/portion.go
@@ -33,13 +33,27 @@ func (po *Portion) CheckNoSplitSells(ticker string) {
 	}
 }
 
+func (po Portion) Alpha() CValue {
+	if po.YieldMarket == 0 {
+		return NewCValue(0, "RUB")
+	}
+	return po.Balance.Mult(1 - po.YieldMarket/po.Yield)
+}
+
 func (po Portion) String() string {
 	date := "----/--/--"
 	if po.IsClosed {
 		date = po.Close.Date.Format("2006/01/02")
 	}
 
+	benchString := func(po Portion) string {
+		if po.YieldMarket == 0 { // TODO not right, might have been real 0
+			return ""
+		}
+		return fmt.Sprintf(", market %.1f%%, alpha %s", po.YieldMarket, po.Alpha())
+	}
+
 	return fmt.Sprintf(
-		"%s: %s (%.1f%%, annual %.1f%%, market %.1f%%)",
-		date, po.Balance, po.Yield, po.YieldAnnual, po.YieldMarket)
+		"%s: %s (%.1f%%, annual %.1f%%%s)",
+		date, po.Balance, po.Yield, po.YieldAnnual, benchString(po))
 }
